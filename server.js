f9541e928a2a2d93e40bc135f80af93cc8eeeed4
Lina Zhu <lina.zhu0212@gmail.com>
2022-06-07 17:12:20 -0400

    replaced an equal sign with a colon

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(express.json());
 // parse requests of content type - application/x-www-form-urlencoded
-app.use(express.urlencoded({extended = true }));
+app.use(express.urlencoded({extended: true }));
 // simple route
 app.get("/", (req, res) => {
     res.json({message: "Welcome to Cardle"});
