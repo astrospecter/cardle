338a4eb2a806fa762c11283c111f33df10a95800
Lina Zhu <lina.zhu0212@gmail.com>
2022-06-21 15:43:20 -0400

    initial implementation of car.findbyid

--- models/model.js
+++ models/model.js
@@ -8,3 +8,19 @@ const Car = function(car) {
     this.country = car.country;
     this.cylinders = car.cylinders;
 };
+Car.findById = (id, result) => {
+    sql.query(`SELECT * FROM carsdb WHERE id = ${id}`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+        if (res.length) {
+            console.log("found car: ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+        // if not found car with the id
+        result({kind: "not_found"}, null);
+    });
+};
