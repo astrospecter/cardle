bd5349fd6623899f6ba9b0b77294e043bde4e2c8
Lina Zhu <lina.zhu0212@gmail.com>
2022-07-14 17:15:38 -0400

    add submission checks to handlekeyup

--- src/hook/useCardle.js
+++ src/hook/useCardle.js
@@ -12,7 +12,7 @@ const useCardle = (solution) => {
 
     // format a guess into a car object
     const formatGuess = () => {
-
+        console.log('formatting guess - ', currentGuess)
     }
 
     // add a new guess into the guesses state
@@ -40,9 +40,24 @@ const useCardle = (solution) => {
     // if user presses enter, add new guess
     const handleKeyup = () => {
         console.log('key pressed - ', key)
+        
+        // check for guess submission
+        if (key === 'Enter') {
+            // only add if turn is less than 7\
+            if (turn > 7) {
+                console.log('You used all your guesses!') // for debug purposes
+                return
+            }
+            // no duplicates
+            if (history.includes(currentGuess)) {
+                console.log('You already guessed this!') // for debug purposes
+            }
+            // car must exist in database (TODO)
+            formatGuess()
+        }
 
         // check for turns left
-        if (turn > 5) {
+        if (turn > 7) {
             console.log('you used all of your guesses!')
             return
         }
