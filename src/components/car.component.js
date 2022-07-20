05af0ea30e68c5d205fe316a7b22202d5b8c10e9
Lina Zhu <lina.zhu0212@gmail.com>
2022-07-20 15:12:14 -0400

    updated object name to prevent confusion

--- src/components/car.component.js
+++ src/components/car.component.js
@@ -1,23 +1,30 @@
 import React, { Component } from "react";
 import Car from '../models/model.js';
 
-export default class Car extends Component {
+export default class CarObject extends Component {
     constructor(props) {
         super(props);
         
-        this.state = {
-            currentCar: {
-                id: null,
-                make: "",
-                model: "",
-                country: "",
-                cylinders: 0,
-                year: 0
-            }
-        }
+        this.make = "";
+        this.model = "";
+        this.country = 0; // this is the country id, country name will be given by the db
+        this.cylinders = 0;
+        this.year = 0;
     }
 
     componentDidMount() {
         this.getCar(this.props.match.params.id); // TODO: ???
+    }
+
+    updateCarInfo(id) {
+        // updates car details based on the dbs
+    }
+
+    getCountryName() {
+        // get the name of the country
+    }
+
+    getContinentbyCountry() {
+        // get the continent
     }
 }
