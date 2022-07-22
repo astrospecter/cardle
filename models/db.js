f69b9705f69693be2370f31eff75aa661e199d15
Lina Zhu <lina.zhu0212@gmail.com>
2022-07-22 14:18:23 -0400

    first implementation of running the carsdb.sql file

--- models/db.js
+++ models/db.js
@@ -1,8 +1,23 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/db.config.js");
+var fs = require('fs');
+var readline = require('readline');
 const connection = mysql.createConnection({
     host: dbConfig.HOST,
     user: dbConfig.USER,
     password: dbConfig.PASSWORD,
     database: dbConfig.DB
 });
+var rl = readline.createInterface({
+    input: fs.createReadStream('../carsdb.sql'),
+    terminal: false
+});
+rl.on('line', function(chunk) {
+    connection.query(chunk.toString('ascii'), function(err, sets, fields) {
+        if (err) console.log(err);
+    });
+});
+rl.on('close', function() {
+    console.log("finished");
+    connection.end();
+})
