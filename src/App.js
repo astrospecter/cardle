acb25365ae61a2669e60b87439192f07db980287
Lina Zhu <lina.zhu0212@gmail.com>
2022-08-23 13:40:49 -0400

    fixed typo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         const randomSolution = json[Math.floor(Math.random()*json.length)]
         setSolution(randomSolution.name)
       })
-  }, [setSolution]))
+  }, [setSolution])
   
   
   return (
