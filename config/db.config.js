8107eb8361227ec57664195030e1030996299988
Lina Zhu <lina.zhu0212@gmail.com>
2022-08-23 13:36:04 -0400

    deleted unneeded files

--- config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "123456",
-    DB: "carsdb"
-};
--- models/country.model.js
+++
@@ -1,7 +0,0 @@
-const sql = require("./db.js");
-
-// constructor
-const Country = function(country) {
-    this.name = country.name;
-    this.continent = country.continent;
-}
